c0d682c8277af5faedceb19cb6b21a0ae99e1475
Spandan Suthar <117799630+spagdoon0411@users.noreply.github.com>
2025-04-01 18:07:43 -0700

    feat: ask user before running ingestion (#12)

--- deployment/deployment.sh
+++ deployment/deployment.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 bash deployment/resources.sh
-bash deployment/ingestion.sh
+
+read -p "Do you want to run the document ingestion pipeline? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    bash deployment/ingestion.sh
+fi
